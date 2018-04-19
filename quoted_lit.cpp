af102ff1ef3d5badca25971135f75d79e4c12f2d
Jacques <jbeauvoir2011@gmail.com>
2018-04-18 21:23:55 -0700

    Update quoted_lit.cpp

--- quoted_lit.cpp
+++ quoted_lit.cpp
@@ -14,15 +14,15 @@ int SyntacticalAnalyzer::quoted_lit()
 {
          // Variable Declrations 
 	int errors = 0;
-	static set<string> firsts = {LPAREN_T,IDENT_T,NUMLIT_T,STRLIT_T,CONS_T,IF_T,DISPLAY_T,
+	static set<token_type> firsts = {LPAREN_T,IDENT_T,NUMLIT_T,STRLIT_T,CONS_T,IF_T,DISPLAY_T,
 				     NEWLINE_T,LISTOP_T,AND_T,OR_T,NOT_T,DEFINE_T,NUMBERP_T,
 				     SYMBOLP_T,LISTP_T,STRINGP_T,ZEROP_T,NULLP_T,PLUS_T,MINUS_T,
 				     MULT_T,DIV_T,MODULO_T,EQUALTO_T,LT_T,GT_T,LTE_T,GTE_T,QUOTE_T,
 				     COND_T,ELSE_T,RPAREN_T};
-	static set<string> follows = {IDENT_T,LPAREN_T,NUMLIT_T,STRLIT_T,QUOTE_T,RPAREN_T};
+	static set<token_type> follows = {IDENT_T,LPAREN_T,NUMLIT_T,STRLIT_T,QUOTE_T,RPAREN_T};
 
-	set<string>::iterator itr1 = firsts.find(token);
-	set<string>::iterator itr2 = follows.find(token);
+	set<token_type>::iterator itr1 = firsts.find(token);
+	set<token_type>::iterator itr2 = follows.find(token);
 
 	p2file << "Entering quoted_lit function; current token is: " 
 	       << lex->GetTokenName (token) << endl;
