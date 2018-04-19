bd46de525bb620ced47e90e9ab2afba1d4679a4e
jbeauvoir <jbeauvoir2011@gmail.com>
2018-04-18 17:28:33 -0700

    functions up to stmt_pair_body done. check lambda rules and start on action

--- else_part.cpp
+++ else_part.cpp
@@ -1,16 +1,39 @@
+/************************************************************
+File: else_part.cpp
+Author: Jacques Beauvoir & Aaron Pineda
+Description: This file has the else_part 
+non-terminal function assoc. with rule 18 and 19.
+
+18. <else_part> -> <stmt>
+19. <else_part> -> lambda
+
+Note: check rule 19 for lambda rule 
+************************************************************/
 #include "SyntacticalAnalyzer.h"
 
 int SyntacticalAnalyzer::else_part()
 {
-
-	int errors = 0;
-	p2file << "Entering else_part function; current token is: " 
-		<< lex->GetTokenName (token) << endl;
-
-	p2file << "Using Rule 18,19" << endl;
-
+        // Variable Declaration
+ 	int errors = 0;
 	static set<string> firsts = {IDENT_T,LPAREN_T,NUMLIT_T,STRLIT_T,QUOTE_T,RPAREN_T};
 	static set<string> follows = {RPAREN_T};
+	set<string>::iterator itr1 = firsts.find(token);
+	set<string>::iterator itr2 = follows.find(token);
+
+	p2file << "Entering else_part function; current token is: " 
+		<< lex->GetTokenName (token) << endl;
+	
+	// RULE 18
+	// one of the firsts terminals is present
+	if ( itr1 != firsts.end() ) {
+	  p2file << "Using Rule 18" << endl;
+	  errors += stmt(); 
+	}
+	// RULE 19 
+	else if() {
+	  p2file << "Using Rule 19" << endl; 
+	}
+	
 
 	p2file << "Exiting else_part function; current token is: " 
 		<< lex->GetTokenName (token) << endl;
--- stmt_pair_body.cpp
+++ stmt_pair_body.cpp
@@ -1,16 +1,67 @@
+/************************************************************
+File: stmt_pair_body.cpp
+Author: Jacques Beauvoir & Aaron Pineda
+Description: This file has the  stmt_pair_body
+non-terminal function assoc. with rule 22 and 23.
+
+22. <stmt_pair_body> -> <stmt> <stmt> RPAREN_T <stmt_pair> 
+23. <stmt_pair_body> -> ELSE_T <stmt> RPAREN_T
+************************************************************/
 #include "SyntacticalAnalyzer.h"
 
 int SyntacticalAnalyzer::stmt_pair_body()
 {
-
+        // Variable Declaration
 	int errors = 0;
-	p2file << "Entering stmt_pair_body function; current token is: " 
-		<< lex->GetTokenName (token) << endl;
-
-	p2file << "Using Rule 22,23" << endl;
-
 	static set<string> firsts = {ELSE_T,IDENT_T,LPAREN_T,NUMLIT_T,STRLIT_T,QUOTE_T};
 	static set<string> follows = {RPAREN_T};
+	set<string>::iterator itr1 = firsts.find(token);
+
+	p2file << "Entering stmt_pair_body function; current token is: " 
+	       << lex->GetTokenName (token) << endl;
+	
+
+	// So for the next few if-else blocks you'll notice
+	// that rule 23 precedes 22. I did this because I 
+	// didn't want to type out a long OR ||  statement. By 
+	// checking for ELSE_T first, I can simply do a set check
+	// with stmt_pair_body's firsts.
+
+	// RULE 23
+	if( token == ELSE_T ) {
+	  
+	  p2file << "Using Rule 23" << endl;
+	  errors += stmt();
+	  // terminal after <stmt>
+	  if( token == RPAREN_T ) {
+	    return errors++;
+	  }
+	  else {
+	    cout << "error in stmt_pair_body for RPAREN" << endl;
+	  }
+	  
+	}
+	// RULE 22 
+	else if ( itr1 != firsts.end() ) {
+	  p2file << "Using Rule 22" << endl;
+	  
+	  errors += stmt();
+	  errors += stmt();
+	  
+	  // Non-terminal check after <stmt>
+	  if ( token == RPAREN_T ) {
+	    errors += stmt_pair();
+	  }
+	  else{
+	    cout << "error in error in stmt_pair_body for RPAREN rule 23" << endl;
+	    return errors++;
+	  }
+	}
+	// error stuff
+	else {
+	  cout << "neither rule 22 or 23 called" << endl;
+	}
+
 
 	p2file << "Exiting stmt_pair_body function; current token is: " 
 		<< lex->GetTokenName (token) << endl;
