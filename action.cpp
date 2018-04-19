c85531ee535d94149a0a9b751c4e1a80df7a9475
Jacques <jbeauvoir2011@gmail.com>
2018-04-18 21:24:43 -0700

    Update action.cpp

--- action.cpp
+++ action.cpp
@@ -38,12 +38,12 @@ int SyntacticalAnalyzer::action()
 {
 
 	int errors = 0;
-	static set<string> firsts = {IF_T,COND_T,LISTOP_T,CONS_T,AND_T,
+	static set<token_type> firsts = {IF_T,COND_T,LISTOP_T,CONS_T,AND_T,
 				     OR_T,NOT_T,NUMBERP_T,SYMBOLP_T,LISTP_T,ZEROP_T,
 				     NULLP_T,STRINGP_T,PLUS_T,MINUS_T,DIV_T,MULT_T,
 				     MODULO_T,EQUALTO_T,GT_T,LT_T,LTE_T,GTE_T,IDENT_T,
 				     DISPLAY_T,NEWLINE_T};
-	static set<string> follows = {RPAREN_T};
+	static set<token_type> follows = {RPAREN_T};
 
 
 	p2file << "Entering action function; current token is: " 
